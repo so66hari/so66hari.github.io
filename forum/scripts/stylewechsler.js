65b15f3ee13854cf8417aa04ed9304cc8d94efa0
so66hari <76953444+so66hari@users.noreply.github.com>
2022-08-22 17:55:00 +0200

    created stylewechsler js

---
+++ forum/scripts/stylewechsler.js
@@ -0,0 +1,62 @@
+var type = getCookie('Vorlage1');
+var wahl = document.getElementById('wahl');
+wahl.innerHTML ='<select onchange="changeskin(this.options[this.selectedIndex].value);window.location.reload();"><option>Wähle einen Style</option><option value="wert1"> Original </option><option value="wert2"> NebelClan </option><option value="wert3"> DonnerClan </option><option value="wert4"> FlussClan </option><option value="wert5"> SchattenClan </option><option value="wert6"> WindClan </option><option value="wert7"> WolkenClan </option></select>';
+        
+switch(type) {
+    case 'wert1':
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/original.css">');
+        break;
+    case 'wert2':
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/nebelclan.css">'); 
+       break; 
+    case 'wert3':
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/donnerclan.css">');
+        break;    
+    case 'wert4':
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/flussclan.css">');
+        break;
+    case 'wert5':
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/schattenclan.css">');
+        break;
+    case 'wert6': 
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/windclan.css">');
+        break;
+    case 'wert7':
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/wolkenclan.css">');
+        break;
+    default:
+        document.write('<link rel="stylesheet" type="text/css" href="/forum/styles/original.css">');
+}
+
+function changeskin(change) {
+    var scheme = change;
+    var name = 'Vorlage1';
+    var pathname = location.pathname;
+    //var myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
+    var ExpDate = new Date ();
+    ExpDate.setTime(ExpDate.getTime() + (180 * 24 * 3600 * 1000));
+    setCookie(name,scheme,ExpDate,'/');
+}
+
+function getCookie(name){
+    var cname = name + "=";
+    var dc = document.cookie;
+    if (dc.length > 0) {
+        begin = dc.indexOf(cname);
+        if (begin != -1) {
+            begin += cname.length;
+            end = dc.indexOf(";", begin);
+            if (end == -1) end = dc.length;
+            return unescape(dc.substring(begin, end));
+        }
+    }
+    return null;
+}
+
+function setCookie(name, value, expires, path, domain, secure) {
+    document.cookie = name + "=" + escape(value) +
+    ((expires == null) ? "" : "; expires=" + expires.toGMTString()) +
+    ((path == null) ? "" : "; path=" + path) +
+    ((domain == null) ? "" : "; domain=" + domain) +
+    ((secure == null) ? "" : "; secure");
+}
