28d28aaf2874c953e3404ef2608027b9c1762a01
so66hari <76953444+so66hari@users.noreply.github.com>
2022-08-24 21:40:20 +0200

    bugfixes

--- forum/pages/buttons.js
+++ forum/pages/buttons.js
@@ -1,8 +1,7 @@
 function changeStyle(input) {
     const background = document.body;
-    const bodyline = document.getElementById('bodyline');
+    const bodyline = document.querySelector('#bodyline');
     const header = document.getElementById('i_logo');
-    const test = document.getElementById('test');
     const tables = document.querySelectorAll('table');
     const tableHead = document.querySelectorAll('th');
     const titles = document.querySelectorAll('h2');
@@ -25,9 +24,8 @@ function changeStyle(input) {
             background.style = "background: url('https://i43.servimg.com/u/f43/19/81/66/84/windcl10.jpg'), #759cc5;";
             bodyline.stlye = "box-shadow: 0px 0px 10px #cccccc; background: linear-gradient(to right, #b78d67, #4b699b); -webkit-box-shadow: 0px 0px 10px #cccccc; border-top-left-radius: 300px 50px; border-top-right-radius: 300px 50px;";
             header.style = "content: url('https://cdn.discordapp.com/attachments/427137355976146951/1004858072776314880/windclanhd.png');";
-            test.style = "margin: 5px 0; border-collapse: collapse; background-color: transparent; border: 1px solid transparent; border-image: linear-gradient(to right, #b78d67, #4b699b); border-image-slice: 1;";
-            for(let i = 0; i < tables.length; i++) {tables[i].classList.add("wctable");}
-            for(let i = 0; i < tableHead.length; i++) {tableHead[i].classList.add("wcth");}
+            for(let i = 0; i < tables.length; i++) {tables[i].className = "wctable";}
+            for(let i = 0; i < tableHead.length; i++) {tableHead[i].className = "wcth";}
             for(let i = 0; i < titles.length; i++) {titles[i].className = "wch2";}
             for(let i = 0; i < text.length; i++) {text[i].className = "wctd";}
             for(let i = 0; i < link.length; i++) {link[i].className = "wca";}
